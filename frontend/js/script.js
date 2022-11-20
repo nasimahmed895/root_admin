c71ec56c3ec2469a93533cb3e4e6147716b6487e
nasimahmed895 <subhoislam624@gmail.com>
2022-11-21 01:40:41 +0600

    new

---
+++ frontend/js/script.js
@@ -0,0 +1,11 @@
+// function myFunction() {
+//     var toggler = document.getElementById("navbar-toggler");
+//     var toggler2 = document.getElementById("navbar-toggler2");
+//     if (toggler.style.display === "none") {
+//         toggler.style.display = "block";
+//         toggler2.style.display = "none";
+//     } else {
+//         toggler.style.display = "none";
+//         toggler2.style.display = "block";
+//     }
+// }
